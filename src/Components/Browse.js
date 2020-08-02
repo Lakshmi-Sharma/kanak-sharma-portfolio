0dc4e5f21dcf637afc283a1eb612c589876ff039
Lakshmi Sharma <lakshmisharma92@gmail.com>
2020-08-02 12:53:40 -0500

    Basic browse page

---
+++ src/Components/Browse.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Browse extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div> Browse here </div>
+    );
+  }
+} 
+export default Browse;
